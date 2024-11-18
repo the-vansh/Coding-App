731b8f36c6a1efa5afcd06e1592c9ebe176ca37e
the-vansh <Contactvanshsaini@gmail.com>
2024-11-19 00:01:17 +0530

    first commit

--- Backend/dbconnection.js
+++ Backend/dbconnection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+//require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect('mongodb+srv://vs3943943:NiziNPQMVYP8KRfo@coding-app.grqf0.mongodb.net/?retryWrites=true&w=majority&appName=Coding-App');
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
