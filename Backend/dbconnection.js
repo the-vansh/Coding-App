ec2f2fd8e10efecd565f1aed01e98c8ac7aa3efd
the-vansh <126165244+the-vansh@users.noreply.github.com>
2024-11-19 11:42:41 +0530

    Update dbconnection.js

--- Backend/dbconnection.js
+++ Backend/dbconnection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-//require('dotenv').config();
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://vs3943943:NiziNPQMVYP8KRfo@coding-app.grqf0.mongodb.net/');
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
