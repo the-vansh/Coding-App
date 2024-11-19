3fb3a066e963e25a95429a3daa3305c63d4fbedc
the-vansh <126165244+the-vansh@users.noreply.github.com>
2024-11-19 12:00:53 +0530

    Update dbconnection.js

--- Backend/dbconnection.js
+++ Backend/dbconnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://vs3943943:NiziNPQMVYP8KRfo@coding-app.grqf0.mongodb.net/');
+    await mongoose.connect('mongodb+srv://vs3943943:NiziNPQMVYP8KRfo@coding-app.grqf0.mongodb.net/?retryWrites=true&w=majority&appName=Coding-App');
     console.log('MongoDB Connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
